332542a773ca567556f02063ae350592f37ea708
Timur2311 <58284492+Timur2311@users.noreply.github.com>
2022-06-16 10:15:37 +0500

    Field name lar o'zbekcha qilindi

--- generator/models.py
+++ generator/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class MapStatistics(models.Model):
-    name = models.CharField(max_length=250)
-    file = models.FileField(upload_to='map/', null=True, verbose_name="")
+    name = models.CharField(max_length=250, verbose_name="Nomi")
+    file = models.FileField(upload_to='map/', null=True, verbose_name="Faylni yuklang")
     
     class Meta:
         ordering = ['-id']

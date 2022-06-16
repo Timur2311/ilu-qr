6195e581630f39f893dc7c1bc24f6848fa48997e
Timur2311 <58284492+Timur2311@users.noreply.github.com>
2022-06-16 10:21:43 +0500

    h1 qo'shildi, verbose_name o'zgartirildi

--- generator/models.py
+++ generator/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class MapStatistics(models.Model):
     name = models.CharField(max_length=250, verbose_name="Nomi")
-    file = models.FileField(upload_to='map/', null=True, verbose_name="Faylni yuklang")
+    file = models.FileField(upload_to='map/', null=True)
     
     class Meta:
         ordering = ['-id']
